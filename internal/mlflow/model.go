a60acafef75b3681173903802bca55b4d6d1bfcf
Burak Demirpolat <burak.demirpolat@trendyol.com>
2023-10-13 00:46:49 +0300

    feat: isolate ml flow model from service response models

--- internal/mlflow/model.go
+++ internal/mlflow/model.go
@@ -2,32 +2,6 @@ package mlflow
 
 import "strings"
 
-type LatestVersion struct {
-	Name         string `json:"name"`
-	Version      string `json:"version"`
-	CurrentStage string `json:"current_stage"`
-	Status       string `json:"status"`
-}
-
-type RegisteredModel struct {
-	Name           string          `json:"name"`
-	LatestVersions []LatestVersion `json:"latest_versions"`
-}
-
-type RegisteredModelsResponse struct {
-	NextPageToken    *string           `json:"next_page_token,omitempty"`
-	RegisteredModels []RegisteredModel `json:"registered_models"`
-}
-
-type ModelVersion struct {
-	Version string `json:"version"`
-}
-
-type ModelVersionsResponse struct {
-	NextPageToken *string        `json:"next_page_token,omitempty"`
-	ModelVersions []ModelVersion `json:"model_versions"`
-}
-
 type Model struct {
 	Name    string
 	Version string
